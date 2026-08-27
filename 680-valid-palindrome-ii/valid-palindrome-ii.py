89c92592293fe34241a9013607e0f8465cd30c60
Saurabh Mishra <sm269790@gmail.com>
2026-08-27 16:49:34 +0530

    Time: 31 ms (83.37%) | Memory: 19.6 MB (15.49%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s)-1
+
+        while l < r:
+            if s [l] != s[r]:
+                skipl,skipr = s[l+1:r+1],s[l:r]
+                return (skipl== skipl[::-1] or skipr == skipr[::-1])
+            r-=1
+            l+=1
+        return True
+
+    
+        
